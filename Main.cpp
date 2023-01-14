68bab3a7abda6d5e56627842a878d87f8a5b13a6
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-01-14 20:18:06 +0200

    Update and rename README.md to Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main() {
+
+	srand(time(NULL));
+	array<double, 10> arr;
+	double sum = 0;
+	cout << "Елементи масива: ";
+
+	for (int i = 0; i < arr.size(); i++) {
+		arr[i] = (rand() % 100 - 50) / 10.0;
+
+		cout << arr[i] << " ";
+
+		if (arr[i] < 0)
+		{
+			sum = sum + arr[i];
+		}
+	}
+	cout << "\nСума вiд ємних елементів: " << sum;
+}
